446105d76f4bec375f896b58de0f6dd706fd395e
Sebastian <sc.ospc@gmail.com>
2022-10-13 12:24:21 -0500

    removed unnecessary variable in front-end js

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,5 @@
 const deleteBtn = document.querySelectorAll('.fa-trash-can')
 const itemPriority = document.querySelectorAll('.item span.fa-flag')
-const notItemPriority = document.querySelectorAll('.item span.priority')
 const checkbox = document.querySelectorAll('input[type="checkbox"]')
 
 Array.from(checkbox).forEach((element) => {
@@ -9,10 +8,6 @@ Array.from(checkbox).forEach((element) => {
 
 Array.from(itemPriority).forEach((element) => {
 	element.addEventListener('click', (e) => markAsPriority(e))
-})
-
-Array.from(notItemPriority).forEach((element) => {
-	element.addEventListener('click', (e) => unmarkAsPriority(e))
 })
 
 Array.from(deleteBtn).forEach((element) => {
@@ -57,25 +52,6 @@ async function markAsPriority(event) {
 	}
 
 }
-
-// async function unmarkAsPriority(event) {
-// 	const itemText = event.target.parentNode.childNodes[3].innerText
-// 	try {
-// 		const response = await fetch('unmarkAsPriority', {
-// 			method: 'put',
-// 			headers: { 'Content-Type': 'application/json' },
-// 			body: JSON.stringify({
-// 				'itemFromJS': itemText
-// 			})
-// 		})
-// 		const data = await response.json()
-// 		console.log(data)
-// 		location.reload()
-
-// 	} catch (err) {
-// 		console.log(err)
-// 	}
-// }
 
 async function deleteItem(event) {
 	const itemId = event.target.parentNode.dataset.id
